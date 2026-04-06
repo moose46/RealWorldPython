80955d515e4cf502ccc415869f7c0afbfc23d611
me <me@robertcurtiss.com>
2026-04-06 12:53:27 -0400

    Init

---
+++ bays.py
@@ -0,0 +1,32 @@
+import sys
+import random
+import itertools
+import numpy as np
+import cv2 as cv
+
+MAP_FILE = 'cape_python.png'
+
+SA1_CORNERS = (130,265,180,315) # (UL-X, UL-Y,LR-x,LR-Y)
+SA2_CORNERS = (180,255,130,305) # (UL-X, UL-Y,LR-x,LR-Y)
+SA3_CORNERS = (105,205,155,255) # (UL-X, UL-Y,LR-x,LR-Y)
+
+class Search():
+    def __init__(self,name):
+        self.name = name
+        self.img = cv.imread(MAP_FILE)
+        if self.img is None:
+            print(f'{self.name} has not been loaded')
+            sys.exit(-1)
+        self.area_actual = 0
+        self.sailor_actual = [0,0] # as local coords within search area
+        self.sa1 = self.img[SA1_CORNERS[1]:SA1_CORNERS[3],SA1_CORNERS[0]:SA1_CORNERS[2]]
+        self.sa2 = self.img[SA2_CORNERS[1]:SA2_CORNERS[3],SA2_CORNERS[0]:SA2_CORNERS[2]]
+        self.sa3 = self.img[SA3_CORNERS[1]:SA3_CORNERS[3],SA3_CORNERS[0]:SA3_CORNERS[2]]
+        self.p1 = .2
+        self.p2 = .5
+        self.p3 = .3
+
+        self.sep1 = 0
+        self.sep2 = 0
+        self.sep3 = 0
+    
